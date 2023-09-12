c78fdda60275e44f667fef12f75f51c1bd278366
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2023-09-12 16:57:38 +0800

    create navbar

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    
+    <!-- NAVBAR Start -->
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a class="text-3xl font-mono" href="">car rental</a>
+    </header>
+
+    <!-- NAVBAR End -->
   </div>
 </template>
