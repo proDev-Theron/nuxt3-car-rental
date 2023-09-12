642ccf1e92765b4fa13ad59258db7d903c551bb4
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2023-09-12 17:34:21 +0800

    Create Homepage Hero

--- app.vue
+++ app.vue
@@ -4,7 +4,18 @@
     <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
       <a class="text-3xl font-mono" href="">car rental</a>
     </header>
-
     <!-- NAVBAR End -->
+
+    <!-- HOME HERO Start-->
+    <div class="relative h-screen w-full bg-no-repeat bg-cover bg-bottom bg-[url('https://images.pexels.com/photos/120049/pexels-photo-120049.jpeg')]">
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="font-mono font-bold text-stone-200 text-5xl">Rent Your Adventure Today</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
+
+    <!-- HOME HERO End-->
   </div>
 </template>
