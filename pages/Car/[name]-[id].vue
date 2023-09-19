65da70704bfb0602f6e77852b56445bb59de7c53
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2023-09-19 14:57:57 +0800

    create 404 error when car ID is not found

--- pages/Car/[name]-[id].vue
+++ pages/Car/[name]-[id].vue
@@ -2,6 +2,7 @@
 const route = useRoute();
 const { toTitleCase } = useUtilities();
 const { cars } = useCars();
+
 useHead({
     title: toTitleCase(route.params.name),
 });
@@ -14,15 +15,22 @@ const car = computed(() => {
     return cars.find((c) => {
         return c.id === parseInt(route.params.id);
     })
-    });
+});
+
+if (!car.value) {
+    throw createError({
+        statusCode: 404,
+        message: `Car with ID ${route.params.id} does not exist`
+    })
+}
 
 </script>
 
 <template>
     <div v-if="car">
-        <CarDetailHero :car="car"/>
-        <CarDetailAttributes :features="car.features"/>
-        <CarDetailDescription :description="car.description"/>
+        <CarDetailHero :car="car" />
+        <CarDetailAttributes :features="car.features" />
+        <CarDetailDescription :description="car.description" />
         <CarDetailContact />
     </div>
 </template>
