a1e60212a7e9b0d8c928bbe880eb5e6cb3a90626
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2023-09-19 13:37:53 +0800

    Add dynamic error handling

---
+++ error.vue
@@ -0,0 +1,14 @@
+<script setup>
+const error = useError();
+const handleError = () => {
+    navigateTo("/");
+}
+
+</script>
+<template>
+    <div class="flex h-screen justify-center items-center flex-col">
+        <h1 class="text-9xl text-blue-500 font-bold">{{error.statusCode}}</h1>
+        <p class="text-xl">{{error.message}}</p>
+        <button @click="handleError" class="rounded mt-7 text-2xl bg-blue-500 px-7 py-4 text-white">Go to Home</button>
+    </div>
+</template>
