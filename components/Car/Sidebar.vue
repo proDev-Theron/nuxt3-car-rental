9c0adf7659e3151de9422737e05dcde6dabed4cd
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2023-09-19 15:21:42 +0800

    Make Location Modal in Sidebar working

--- components/Car/Sidebar.vue
+++ components/Car/Sidebar.vue
@@ -1,13 +1,36 @@
+<script setup>
+const modal = ref({
+  make: false,
+  location: false,
+  price: false
+})
+
+const city = ref('') // for v-model
+
+const route = useRoute();
+
+const updateModal = (key) => {
+  modal.value[key] = !modal.value[key]
+}
+
+const onChangeLocation = () => {
+  if(city.value == '') return;
+  updateModal('location')
+  navigateTo(`/city/${city.value}/car/${route.params.make}`) 
+  city.value='';
+}
+</script>
+
 <template>
     <div>
         <div class="shadow border w-64 mr-10 z-30 h-[190px]">
             <div class="relative flex justify-between p-5 cursor-pointer border-b">
               <h3>Location</h3>
-              <h3 class="text-blue-400 capitalize">Toronto</h3>
-              <!-- <div class="absolute border shadow left-56 p-5 top-1 m-1 bg-white">
-                <input type="text" class="border p-1 rounded">
-                <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
-              </div> -->
+              <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{ route.params.city }}</h3>
+              <div v-if="modal.location == true" class="absolute border shadow left-56 p-5 top-1 m-1 bg-white">
+                <input type="text" class="border p-1 rounded" v-model="city">
+                <button @click="onChangeLocation" class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
+              </div>
             </div>
             <div class="relative flex justify-between p-5 cursor-pointer border-b">
               <h3>Make</h3>
